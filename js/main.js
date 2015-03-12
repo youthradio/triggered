a770caa95151ba1ced164c1eeaf447a9e3414276
Lo Benichou <lo@Los-iMac.local>
2015-03-12 12:16:30 -0700

    changed intro screen scale

--- js/main.js
+++ js/main.js
@@ -2,38 +2,36 @@ $(function(){
   // create stage
   var stage = new createjs.Stage("mainCanvas");
   var images = []
+  var c = $('#mainCanvas');
+  var ct = c.get(0).getContext('2d');
+  var container = $(c).parent();
 
   var intro_txt = new createjs.Text("In this interactive, you'll explore the stories behind 13 objects that police officers have mistaken for guns. The cases you're about to see vary in circumstance and outcome, but each ended with someone getting shot.", "30px Courier", "#FFFFFF")
       intro_txt.textAlign = "center";
-      intro_txt.y = 170
-      intro_txt.x = $(".canvasContainer").width()/2
+      intro_txt.y = 150
+      intro_txt.x = 660
       intro_txt.lineWidth = $(".canvasContainer").width()/1.5
       intro_txt.lineHeight = 50;
-
-  // var skip_button = new Image();
-  //     skip_button.src = "images/skipintro.png"
-  //     skip_button.onload = addSkipButton;
-
 
   var progress = new createjs.Shape();
   var progressBellow = new createjs.Shape();
   var txt = new createjs.Text();
   var preload = new createjs.LoadQueue();
   var manifest = [
-    {src: 'images/center.png', id: 'center', x: 380, y: 90},
-    {src: 'images/cell.png', id: 'cell', x: 1100, y: 250},
-    {src: 'images/taser.png', id: 'taser', x: 780, y: -10},
-    {src: 'images/brush.png', id: 'brush', x: 780, y: 360},
-    {src: 'images/pizza.png', id: 'pizza', x: 1100, y: 400},
-    {src: 'images/wallet.png', id: 'wallet', x: 1100, y: 10},
-    {src: 'images/wii.png', id: 'wii', x: 50, y: 340},
-    {src: 'images/pill.png', id: 'pill', x: 100, y: 150},
-    {src: 'images/pelletgun_a.png', id: 'pellet_gun_a', x: 180, y: 0},
-    {src: 'images/pelletgun_b.png', id: 'pellet_gun_b', x: 380, y: 430},
-    {src: 'images/waterpistol.png', id: 'water_pistol', x: 600, y: 10},
-    {src: 'images/toygun.png', id: 'toy_gun', x: 880, y: 230},
-    {src: 'images/keys.png', id: 'keys', x: 600, y: 450},
-    {src: 'images/mic.png', id: 'mic', x: 150, y: 120}
+    {src: 'https://youthradio.org/innovationlab/triggered/images/center.png', id: 'center', x: 380, y: 90},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/cell.png', id: 'cell', x: 1100, y: 250},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/taser.png', id: 'taser', x: 780, y: -10},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/brush.png', id: 'brush', x: 780, y: 360},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/pizza.png', id: 'pizza', x: 1100, y: 400},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/wallet.png', id: 'wallet', x: 1100, y: 10},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/wii.png', id: 'wii', x: 50, y: 340},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/pill.png', id: 'pill', x: 100, y: 150},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/pelletgun_a.png', id: 'pellet_gun_a', x: 180, y: 0},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/pelletgun_b.png', id: 'pellet_gun_b', x: 380, y: 430},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/waterpistol.png', id: 'water_pistol', x: 600, y: 10},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/toygun.png', id: 'toy_gun', x: 880, y: 230},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/keys.png', id: 'keys', x: 600, y: 450},
+    {src: 'https://youthradio.org/innovationlab/triggered/images/mic.png', id: 'mic', x: 150, y: 120}
     ]
 
 
@@ -49,14 +47,6 @@ $(function(){
    .wait(1000)
    .call(init)
 
-  // .to({text: "Youth Radio wanted to know the stories behind some of these shootings. But the official data is incomplete. Police shootings aren't reliably reported to federal agencies. This might change with the re-authorization of the Death in Custody Reporting Act, which encourages states to report the deaths of people while in police custody."})
-  // .to({alpha: 1}, 1000, createjs.Ease.sineIn)
-  // .to({alpha: 0}, 20000, createjs.Ease.sineIn)
-  // .to({text: "In some cases, officers shoot when they mistake another object for a gun. Here are some of those high profile stories."})
-  // .to({alpha: 1}, 1000, createjs.Ease.sineIn)
-  // .to({alpha: 0}, 10000, createjs.Ease.sineIn)
-
-
   stage.addChild(intro_txt)
 
   // Get data from google spreadsheet
@@ -67,40 +57,20 @@ $(function(){
     return data
   }
 
-  // add skip button to intro
-  // function addSkipButton(){
-  //   skip_button_bitmap = new createjs.Bitmap(skip_button);
-  //   // skip_button_bitmap.cache(0, 0, skip_button.width, skip_button.height);
-  //   skip_button_bitmap.y = 530
-  //   skip_button_bitmap.x = $(".canvasContainer").width()/1.3
-  //   createjs.Tween.get(skip_button_bitmap, {loop:true}).to({alpha:0}, 1500).to({alpha:1}, 500, createjs.Ease.quadIn);
-  //   skip_button_bitmap.addEventListener("click", function(){
-  //     skip_intro()
-  //   })
-  //   stage.addChild(skip_button_bitmap);
-  //   stage.update();
-  // }
-
-
-
-  //  // onclick, skip the intro text and goes straight to main screen
-  // function skip_intro(){
-  //   createjs.Ticker.setPaused(true);
-  //   stage.removeChild(intro_txt, skip_button_bitmap);
-  //   init()
-  // }
-
   function tick(){
     stage.update()
   }
 
   // canvas resize
   function resize() {
-    // browser viewport size
+    // c.attr('width', $(container).width() ); //max width
+    // c.attr('height', $(container).height() ); //max height
+
+  // browser viewport size
     var w = window.innerWidth;
     var h = window.innerHeight;
 
-    // stage dimensions
+  // stage dimensions
     var ow = 1320; // your stage width
     var oh = 640; // your stage height
 
@@ -138,6 +108,7 @@ $(function(){
       txt.y = 500;
       txt.font = ("30px press_style_extra_lregular");
       txt.color = ("#0B090B");
+      resize();
    }
 
 
@@ -166,6 +137,7 @@ $(function(){
 
     // fades the background
     $("canvas").animate({backgroundColor: 'rgba(0, 0, 0, 0)'}, 2000)
+    $(".container-fluid").addClass("bg-image")
 
     // Gets data from the backend and resolves promise
     getData().then(function(results){
